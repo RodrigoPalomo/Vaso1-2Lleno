d0872af00dacbf944f1ba64cb9fedec926f1b262
Rodrigo Palomo <repmlm@hotmail.com>
2022-04-11 20:47:20 -0300

    Arreglos product detail

--- app.js
+++ app.js
@@ -13,3 +13,6 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
+app.get('/detail', (req, res) =>{
+    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+});
